a57d8e826482fa7bc7f669a244bb25c3fbe520d1
RinRinPARK <surinp8019@naver.com>
2024-07-18 10:59:46 +0900

    Fix: category NotBlank에서 NotNull로 변경

--- src/main/kotlin/com/swm_standard/phote/service/QuestionService.kt
+++ src/main/kotlin/com/swm_standard/phote/service/QuestionService.kt
@@ -24,6 +24,9 @@ class QuestionService(
     fun createQuestion(memberId:UUID, request: CreateQuestionRequestDto, imageUrl: String?)
     : CreateQuestionResponseDto {
 
+        println("여기")
+        println(request)
+
         // 문제 생성 유저 확인
         val member = memberRepository.findById(memberId).orElseThrow { NotFoundException("존재하지 않는 member") }
 
