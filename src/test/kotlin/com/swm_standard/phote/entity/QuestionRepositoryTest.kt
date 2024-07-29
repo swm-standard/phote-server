f5128c8f14f9e7ce7e566aac2478c36142af9c43
RinRinPARK <surinp8019@naver.com>
2024-07-29 13:29:26 +0900

    Test: Question Repository 테스트 코드 작성

--- src/test/kotlin/com/swm_standard/phote/entity/QuestionRepositoryTest.kt
+++ src/test/kotlin/com/swm_standard/phote/entity/QuestionRepositoryTest.kt
@@ -2,10 +2,13 @@ package com.swm_standard.phote.entity
 
 import com.swm_standard.phote.repository.QuestionRepository
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException
 import org.springframework.transaction.annotation.Transactional
+import java.util.Optional
 import kotlin.test.assertEquals
 
 @SpringBootTest
@@ -32,5 +35,52 @@ class QuestionRepositoryTest {
 
         // then
         assertEquals(savedQuestion.id, question.id)
+    }
+
+    @Test
+    fun `문제 상세정보를 조회한다`() {
+        // given
+        val question = Question(
+            member = Member("phote", "phote@test.com", "imageUrl", Provider.KAKAO),
+            statement = "모든 각이 동일한 삼각형은?",
+            image = "http://example.com/image.jpg",
+            answer = "정삼각형",
+            category = Category.ESSAY,
+            memo = "삼각형 내각의 합은 180도이다."
+        )
+
+        // when
+        Mockito.`when`(questionRepository.findById(question.id)).thenReturn(Optional.of(question))
+        val savedQuestion = questionRepository.findById(question.id).orElseThrow()
+
+        // then
+        assertEquals(question.member, savedQuestion.member)
+        assertEquals("모든 각이 동일한 삼각형은?", savedQuestion.statement)
+        assertEquals("http://example.com/image.jpg", savedQuestion.image)
+        assertEquals("정삼각형", savedQuestion.answer)
+        assertEquals(Category.ESSAY, savedQuestion.category)
+        assertEquals("삼각형 내각의 합은 180도이다.", savedQuestion.memo)
+    }
+
+    @Test
+    fun `문제를 삭제한다`() {
+        // given
+        val question = Question(
+            member = Member("phote", "phote@test.com", "imageUrl", Provider.KAKAO),
+            statement = "모든 각이 동일한 삼각형은?",
+            image = "http://example.com/image.jpg",
+            answer = "정삼각형",
+            category = Category.ESSAY,
+            memo = "삼각형 내각의 합은 180도이다."
+        )
+
+        // when
+        questionRepository.deleteById(question.id)
+
+        assertThrows<NotFoundException> {
+            questionRepository.findById(question.id).orElseThrow {
+                NotFoundException()
+            }
+        }
     }
 }
