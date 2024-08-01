8fe8158c34b8197bac4f2c1b65f40726c557d761
adorableco <tpdusdl1001@knu.ac.kr>
2024-08-02 01:25:50 +0900

    Test: gradeExam
    part of swm-125
    related to: #135

---
+++ src/test/kotlin/com/swm_standard/phote/entity/AnswerTest.kt
@@ -0,0 +1,71 @@
+package com.swm_standard.phote.entity
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import java.util.UUID
+
+class AnswerTest {
+    @Test
+    fun `문제가 객관식이면 정오답 체크를 하고 틀릴 경우 isCorrect가 false가 된다`() {
+        val category = Category.MULTIPLE
+        val submittedAnswer = "1"
+        val correctAnswer = "5"
+        val answer = createAnswer(category, submittedAnswer, correctAnswer)
+
+        val checkAnswer = answer.isMultipleAndCheckAnswer()
+
+        Assertions.assertTrue(checkAnswer)
+        Assertions.assertFalse(answer.isCorrect)
+    }
+
+    fun createWorkbook(): Workbook =
+        Workbook(
+            title = "hinc",
+            description = null,
+            member = createMember(),
+            emoji = "📚",
+        )
+
+    fun createMember(): Member =
+        Member(
+            name = "Mayra Payne",
+            email = "penelope.mccarty@example.com",
+            image = "dicant",
+            provider = Provider.APPLE,
+        )
+
+    fun createExam() =
+        Exam(
+            member = createMember(),
+            workbook = createWorkbook(),
+            sequence = 4282,
+        )
+
+    fun createAnswer(
+        category: Category,
+        submittedAnswer: String,
+        correctAnswer: String,
+    ) = Answer(
+        question =
+            Question(
+                id = UUID.randomUUID(),
+                member =
+                    Member(
+                        name = "Erik Ashley",
+                        email = "jay.combs@example.com",
+                        image = "per",
+                        provider = Provider.APPLE,
+                    ),
+                statement = "Kentucky",
+                options = null,
+                image = null,
+                answer = correctAnswer,
+                category = category,
+                questionSet = listOf(),
+                memo = null,
+            ),
+        exam = createExam(),
+        submittedAnswer = "euismod",
+        sequence = 2017,
+    )
+}
