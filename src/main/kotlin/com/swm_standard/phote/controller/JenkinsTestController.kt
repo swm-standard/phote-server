3497f659a2be947aa553f65b4b83dfa8a0462e46
adorableco <tpdusdl1001@knu.ac.kr>
2024-08-01 19:37:29 +0900

    Test:  jenkins 설정
    part of swm-106
    related to: #110

---
+++ src/main/kotlin/com/swm_standard/phote/controller/JenkinsTestController.kt
@@ -0,0 +1,11 @@
+package com.swm_standard.phote.controller
+
+import com.swm_standard.phote.common.responsebody.BaseResponse
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class JenkinsTestController {
+    @GetMapping("/test")
+    fun test(): BaseResponse<Unit> = BaseResponse(msg = "test")
+}
