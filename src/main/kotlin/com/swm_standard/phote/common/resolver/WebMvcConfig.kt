8834216aa3eab294732abd42ec00052ad9023738
RinRinPARK <surinp8019@naver.com>
2024-08-04 13:45:27 +0900

    Fix: allowedHeaders 추가

--- src/main/kotlin/com/swm_standard/phote/common/resolver/WebMvcConfig.kt
+++ src/main/kotlin/com/swm_standard/phote/common/resolver/WebMvcConfig.kt
@@ -18,6 +18,7 @@ class WebMvcConfig(
         registry.addMapping("/**")
             .allowedOriginPatterns("*")
             .allowedMethods("*")
+            .allowedHeaders("*")
             .allowCredentials(true)
     }
 }
