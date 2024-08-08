e35c4d8226b40f6c8667cf97947f99ac4bd7a298
adorableco <tpdusdl1001@knu.ac.kr>
2024-08-08 14:05:33 +0900

    Fix: getKaKaoUserInfo 원복
    part of swm-147
    related to: #168

--- src/main/kotlin/com/swm_standard/phote/controller/AuthController.kt
+++ src/main/kotlin/com/swm_standard/phote/controller/AuthController.kt
@@ -7,15 +7,11 @@ import com.swm_standard.phote.service.KaKaoAuthService
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.http.HttpHeaders
-import org.springframework.http.ResponseCookie
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.servlet.view.RedirectView
-import org.springframework.web.util.UriComponentsBuilder
 
 @RestController
 @RequestMapping("/api/auth")
@@ -57,41 +53,11 @@ class AuthController(
     @GetMapping("/kakao-token")
     fun getKakaoUserInfo(
         @RequestParam code: String,
-    ): ResponseEntity<Void> {
-        // 카카오에서 액세스 토큰을 가져옵니다.
+    ): BaseResponse<UserInfoResponse> {
         val accessToken = kaKaoAuthService.getTokenFromKakao(code)
-        // 액세스 토큰을 사용하여 사용자 정보를 가져옵니다.
         val userInfo = kaKaoAuthService.getUserInfoFromKakao(accessToken)
 
-        // 쿠키에 액세스 토큰을 저장합니다.
-        val tokenCookie =
-            ResponseCookie
-                .from("accessToken", accessToken)
-                .httpOnly(true)
-                .secure(true)
-                .path("/")
-                .maxAge(7 * 24 * 60 * 60) // 7일 동안 유효
-                .build()
-
-        // 리다이렉트 URL 생성
-        val redirectUrl =
-            if (userInfo.isMember == false) {
-                UriComponentsBuilder
-                    .fromUriString("https://pho-te.com/workbook")
-                    .build()
-                    .toUriString()
-            } else {
-                UriComponentsBuilder
-                    .fromUriString("https://pho-te.com/workbook")
-                    .build()
-                    .toUriString()
-            }
-
-        // 리다이렉트 및 쿠키 설정
-        return ResponseEntity
-            .status(302)
-            .header(HttpHeaders.LOCATION, redirectUrl)
-            .header(HttpHeaders.SET_COOKIE, tokenCookie.toString())
-            .build()
+        val message = if (userInfo.isMember == false) "회원가입 성공" else "로그인 성공"
+        return BaseResponse(msg = message, data = userInfo)
     }
 }
