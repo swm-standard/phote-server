b14c4fee9b8214b4eb7fb3d14c5e10194494c2d0
adorableco <tpdusdl1001@knu.ac.kr>
2024-08-10 12:19:25 +0900

    Test: checkMultipleAnswer() 변경에 따른 테스트 수정
    part of swm-152
    related to: #180

--- src/test/kotlin/com/swm_standard/phote/entity/AnswerTest.kt
+++ src/test/kotlin/com/swm_standard/phote/entity/AnswerTest.kt
@@ -6,40 +6,15 @@ import java.util.UUID
 
 class AnswerTest {
     @Test
-    fun `문제가 객관식이면 정오답 체크를 하고 틀릴 경우 isCorrect가 false가 된다`() {
+    fun `문제가 객관식이면 정오답 체크한다`() {
         val category = Category.MULTIPLE
         val submittedAnswer = "1"
         val correctAnswer = "5"
         val answer = createAnswer(category, submittedAnswer, correctAnswer)
 
-        val checkAnswer = answer.isMultipleAndCheckAnswer()
+        answer.checkMultipleAnswer()
 
-        Assertions.assertTrue(checkAnswer)
-        Assertions.assertFalse(answer.isCorrect)
-    }
-
-    @Test
-    fun `문제가 주관식이면 false를 반환한다`() {
-        val category = Category.ESSAY
-        val submittedAnswer = "essay test"
-        val correctAnswer = "essay test"
-        val answer = createAnswer(category, submittedAnswer, correctAnswer)
-
-        val checkAnswer = answer.isMultipleAndCheckAnswer()
-
-        Assertions.assertFalse(checkAnswer)
-    }
-
-    @Test
-    fun `답안이 null이면 isCorrect는 false로 체크한다`() {
-        val category = Category.MULTIPLE
-        val submittedAnswer = null
-        val correctAnswer = "5"
-        val answer = createAnswer(category, submittedAnswer, correctAnswer)
-
-        val checkAnswer = answer.isMultipleAndCheckAnswer()
-
-        Assertions.assertTrue(checkAnswer)
+        Assertions.assertEquals(submittedAnswer == correctAnswer, answer.isCorrect)
         Assertions.assertFalse(answer.isCorrect)
     }
 
@@ -73,23 +48,23 @@ class AnswerTest {
         correctAnswer: String,
     ) = Answer(
         question =
-        Question(
-            id = UUID.randomUUID(),
-            member =
-            Member(
-                name = "Erik Ashley",
-                email = "jay.combs@example.com",
-                image = "per",
-                provider = Provider.APPLE,
+            Question(
+                id = UUID.randomUUID(),
+                member =
+                    Member(
+                        name = "Erik Ashley",
+                        email = "jay.combs@example.com",
+                        image = "per",
+                        provider = Provider.APPLE,
+                    ),
+                statement = "Kentucky",
+                options = null,
+                image = null,
+                answer = correctAnswer,
+                category = category,
+                questionSet = listOf(),
+                memo = null,
             ),
-            statement = "Kentucky",
-            options = null,
-            image = null,
-            answer = correctAnswer,
-            category = category,
-            questionSet = listOf(),
-            memo = null,
-        ),
         exam = createExam(),
         submittedAnswer = submittedAnswer,
         sequence = 2017,
